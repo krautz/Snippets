d77e8fae2f428ff078993c41ab21c7c55c7ada5b
Caio Krauthamer <caio_k@hotmail.com>
2023-04-07 16:35:10 -0300

    Pyhton: Add installments payment utility

---
+++ python/README.md
@@ -0,0 +1,3 @@
+# Python Snippets
+
+- installments_payment.py -> script to determine if it is worth it to pay in installments or no installments (with discount)
---
+++ python/installments_payment.py
@@ -0,0 +1,26 @@
+def installments_payment(
+    installments_price,
+    installments_amount,
+    discount_price,
+    monthly_return = 0.01
+):
+    profit = 0
+    installment_price = installments_price / installments_amount
+    installments_original_price = installments_price
+    for _ in range(installments_amount):
+        month_profit = installments_price * monthly_return
+        profit += month_profit
+        installments_price -= (installment_price - month_profit)
+
+    discount = installments_original_price - discount_price
+    print(f"Discount: {(discount / installments_original_price) * 100:.2f}%")
+    print(f"Installments Price: {installments_original_price}")
+    print(f"Profit: {profit:.2f}")
+    print(f"Installments Price Less Profit: {installments_original_price - profit:.2f}")
+    print(f"Discount Price: {discount_price}")
+    if installments_original_price - profit < discount_price:
+        print("Better to pay in installments")
+    elif installments_original_price - profit > discount_price:
+        print("Better to pay with discount")
+    else:
+        print("No difference between payling with discount or in installments")
